a49b5c6427f609bdde3ad3d19fcaddf80e9ce54e
MatheusEstanislau <matheus.estanislau7@gmail.com>
2020-05-05 02:21:02 -0300

    Aula 36: Tornando membros privados.

--- pergunta_app/lib/main.dart
+++ pergunta_app/lib/main.dart
@@ -2,61 +2,51 @@ import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
-
-class PerguntaAppState extends State<PerguntaApp> {
-  var perguntaSelecionada = 0;
-
-  void responder() {
-    if (perguntaSelecionada == 1) {
-      setState(() {
-        perguntaSelecionada =0;
-      });
-    }
-    else {
-      setState(() {
-        perguntaSelecionada++;
-      });
-    }
-    print(perguntaSelecionada);
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
   }
+}
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaselecionada = 0;
+  void _responder() {
+    setState(() {
+      _perguntaselecionada++;
+    });
+    print(_perguntaselecionada);
+  }
+
+  final List<String> perguntas = [
+    'Qual sua cor favorita ?',
+    'Qual seu animal favorito?'
+  ];
 
   @override
   Widget build(BuildContext context) {
-
-    const List<String> perguntas = [
-      'Qual é a sua cor favorita ?',
-      'Qual é o seu animal favorito ?'
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Perguntas'),
+          title: Text("Perguntas"),
         ),
         body: Column(
           children: <Widget>[
-            Text(perguntas[perguntaSelecionada]),
+            Text(perguntas.elementAt(_perguntaselecionada)),
             RaisedButton(
-              child: Text('Resposta 1'),
-              onPressed: responder,
+              child: Text("Resposta 1"),
+              onPressed: _responder,
             ),
             RaisedButton(
-              child: Text('Resposta 2'),
-              onPressed: responder,
+              child: Text("Resposta 2"),
+              onPressed: _responder,
             ),
             RaisedButton(
-              child: Text('Resposta 3'),
-              onPressed: responder,
+              child: Text("Resposta 3"),
+              onPressed: _responder,
             ),
           ],
         ),
       ),
     );
-  }
-}
-
-class PerguntaApp extends StatefulWidget {
-  PerguntaAppState createState() {
-    return PerguntaAppState();
   }
 }
